0ea389efc42ceff276e9d8edddde78f0c9da9dcf
Faduma <fadumascode@gmail.com>
2022-10-30 08:28:32 +1100

    Made mobile responsive - added hamburger menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('open')
+})
+
+document.querySelector('.close-btn').addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('open')
+})
